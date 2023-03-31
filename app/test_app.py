782d5dbafcf28eee8c5a50f161eadd8f230096e4
Nafise <73890085+Nafise-Dev@users.noreply.github.com>
2023-03-31 16:53:55 +0200

    Update test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -5,6 +5,7 @@ class TestTasks(unittest.TestCase):
     
     def test_tasks(self):
         expected_results = [{'name': 'Task 1', 'completed': 'Yes'}, {'name': 'Task 1', 'completed': 'No'}]
+        self.assertEqual(tasks(), expected_results)
         
 if __name__ == '__main__':
     unittest.main()
